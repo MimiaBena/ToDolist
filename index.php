27d53d3872a831eca2ec3423b1aa1d7a97489f47
Mokhtaria Benaouali <benaoualimokhtaria@hotmail.com>
2021-11-05 15:35:50 +0100

    modification fichier php

--- index.php
+++ index.php
@@ -33,9 +33,31 @@ $error="";
             <input type="text" name="tache" class="tache_input">
             <button type="submit"
              name="submit" id="button_add" class="button_add">Ajouter Tache</button>  
-           
-        
         </form>
+        <table>
+            <thead>
+                <tr>
+                    <th>Numéro</th>
+                    <th>Tâches</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+               
+            <tbody>
+                <?php
+                  $taches =mysqli_query($database, "SELECT * FROM taches");
+                $num=1;
+                  while ($row = mysqli_fetch_array($taches))  { ?>
+                <tr>
+                    <td><?php echo $num; ?></td>
+                    <td class="tache"> <?php echo $row['tache']; ?> </td>
+                    <td class="supprimer"><a href="index.php?del_task=<?php echo $row['id'] ?>">x</a></td>
+                </tr>
+                <?php $num++; } ?>
+            </tbody>
+        
+        
+        </table>
     </body>
 
 </html>
