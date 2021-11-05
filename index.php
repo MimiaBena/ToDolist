1ded7b4669eeec928119728ecb7b0df930912de4
Mokhtaria Benaouali <benaoualimokhtaria@hotmail.com>
2021-11-05 11:59:07 +0100

    1ere version html

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> ToDolist</title>
+    </head>
+    <body>
+       <h1>ToDo List<span id="point">.</span></h1>
+        <form method="post" action="index.php" class="input_form"> 
+            <input type="text" name="tache" class="tache_input">
+            <button type="submit"
+             name="button" id="button_add" class="button_add">Ajouter Tache</button>     
+        
+        </form>
+    </body>
+
+</html>
