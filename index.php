09b5d2e07bf108d8fe06a460a62f6cbe41bbd0d0
Mokhtaria Benaouali <benaoualimokhtaria@hotmail.com>
2021-11-05 12:54:10 +0100

    modifier Css

--- index.php
+++ index.php
@@ -1,14 +1,17 @@
+
+
 <!DOCTYPE html>
 <html>
     <head>
         <title> ToDolist</title>
+        <link rel="stylesheet" type="text/css" href="todolist_css.css">
     </head>
     <body>
        <h1>ToDo List<span id="point">.</span></h1>
         <form method="post" action="index.php" class="input_form"> 
             <input type="text" name="tache" class="tache_input">
             <button type="submit"
-             name="button" id="button_add" class="button_add">Ajouter Tache</button>     
+             name="submit" id="button_add" class="button_add">Ajouter Tache</button>     
         
         </form>
     </body>
