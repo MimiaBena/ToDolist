43ed0651c5b3abc3935a3051499e0bd6a5a791fd
Mokhtaria Benaouali <benaoualimokhtaria@hotmail.com>
2021-11-06 12:08:08 +0100

    modifier la fonction update

--- index.php
+++ index.php
@@ -1,4 +1,52 @@
+<?php
+$error="";
+$update = false;
+    //connecter à notre base
+    $database=mysqli_connect("localhost", "root", "", "todoliste");
+    
+    //condition du button
+    if(isset($_POST['submit'])){
+          if(empty($_POST['tache'])){
+      $error=" Vous devez remplir la tache, SVP.";
+      } else{
+         $tache=$_POST['tache'];
+         $sql ="INSERT INTO taches (tache) VALUES ('$tache')";
+        mysqli_query($database, $sql);
+    
+      }
+         
+    }
+    // delete tach
+if (isset($_GET['supprimer_tache'])) {
+	$id = $_GET['supprimer_tache'];
 
+	mysqli_query($database, "DELETE FROM taches WHERE id=".$id);
+	header('location: index.php');
+}
+//modifier tache
+if(isset($_GET['modifier_tache'])){
+    $id =$_GET['modifier_tache'];
+    $update = true;
+		$record = mysqli_query($database, "SELECT * FROM taches WHERE id=".$id);
+
+		
+			$n = mysqli_fetch_array($record);
+			$tache = $n['tache'];
+			
+		
+}
+
+if (isset($_POST['update'])) {
+	$id = $_POST['id'];
+	$tache = $_POST['tache'];
+	
+
+	mysqli_query($database, "UPDATE taches SET tache='$tache' WHERE id=$id");
+	$_SESSION['message'] = "tache updated!"; 
+	header('location: index.php');
+}
+
+?>
 
 <!DOCTYPE html>
 <html>
@@ -8,7 +56,7 @@
     </head>
     <body>
        <h1>ToDo List<span id="point">.</span></h1>
-        <form method="post" action="action.php" class="input_form"> 
+        <form method="post" action="index.php" class="input_form"> 
              <?php if(isset($error)){?>
             <p><?php echo $error; ?></p>
                   
